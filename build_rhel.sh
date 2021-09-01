20f8d5fce3d24d9424e37e4b47e28a0bc3dd9b3b
vibhutisawant <41043754+vibhutisawant@users.noreply.github.com>
2021-09-01 12:43:39 +0530

    Update build_rhel.sh

--- build_rhel.sh
+++ build_rhel.sh
@@ -10,17 +10,7 @@ set -e -o pipefail
 PACKAGE_NAME="docker"
 PACKAGE_VERSION="20.10.8"
 CONTAINERD_VERSION="1.4.9"
-#DOCKER-CE_UPLOAD_PATH="docker-ce-20.10-8"
-#CONTAINERD_UPLOAD_PATH="containerd-1.4-9"
 PATCH_URL="https://raw.githubusercontent.com/vibhutisawant/taro-ui/next/patch/"
-#Git Token needed to download files from github.ibm.com
-GIT_TOKEN=""
-##API KEY from the Service Credentials shared with read-write access. IAM Authentication is used here.
-API_KEY=""
-##Name of the bucket in IBM Cloud Storage
-BUCKET_NAME=""
-##Public Endpoint where bucket exists
-PUBLIC_ENDPOINT=""
 CURDIR="$(pwd)"
 FORCE="false"
 LOG_FILE="$CURDIR/logs/${PACKAGE_NAME}-${PACKAGE_VERSION}-$(date +"%F-%T").log"
